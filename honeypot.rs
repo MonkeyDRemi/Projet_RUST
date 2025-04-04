3926ffbd189d753d1474c4afb200ceab993e8788
MonkeyDRemi <remi.vavichandran2@gmail.com>
2025-04-04 20:32:03 +0200

    Initialisation du projet honeypot

--- honeypot.rs
+++ honeypot.rs
@@ -1,1 +1,16 @@
-#debut
+use std::net::{TcpListener, TcpStream};
+use std::thread;
+use std::io::{Write, BufReader, BufRead, Read};
+use std::fs::OpenOptions;
+use std::time::{SystemTime, UNIX_EPOCH};
+use std::collections::HashMap; 
+
+fn handle_client_ssh(mut stream: TcpStream) {
+   
+}
+
+fn main() -> std::io::Result<()> {
+    println!("[*] Démarrage du honeypot SSH...");
+    
+    Ok(())
+}
