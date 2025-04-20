1967a392968f8d1b35c4699fb617e05ba70152e0
MonkeyDRemi <remi.vavichandran2@gmail.com>
2025-04-20 21:37:08 +0200

    Ajout de la validation simple des identifiants SSH

--- honeypot.rs
+++ honeypot.rs
@@ -6,7 +6,19 @@ use std::time::{SystemTime, UNIX_EPOCH};
 use std::collections::HashMap; 
 
 fn handle_client_ssh(mut stream: TcpStream) {
-   
+   buffer.clear();
+    reader.read_line(&mut buffer).expect("Erreur lecture mdp");
+    let password = buffer.trim().to_string();
+
+    if login != "admin" || password != "admin-techpro" {
+        stream.write_all(b"Permission denied, please try again.\r\n").unwrap();
+        println!("[-] Identifiants incorrects pour {}/{}", login, password);
+        return; 
+    }
+
+    stream.write_all(b"\r\nWelcome to Ubuntu 20.04.6 LTS (GNU/Linux 5.4.0-42-generic x86_64)\r\n\r\n").unwrap();
+    stream.flush().unwrap();
+    println!("[+] Authentification réussie pour {}", login);
 }
 
 fn main() -> std::io::Result<()> {
@@ -14,3 +26,4 @@ fn main() -> std::io::Result<()> {
     
     Ok(())
 }
+
